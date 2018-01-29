a20510d5f399c01f99768a5162c8e304f2a8777c
Gabriel Thomas <gabrieljjthomas@gmail.com>
2018-01-29 17:52:20 -0500

    Beer song methods completed

--- src/BeerSong.js
+++ src/BeerSong.js
@@ -1,7 +1,17 @@
+Main();
+
 function Main() {
-    return 'Main function';
+    Ninety_Nine_Bottles_of_Beer();
 }
 
 function Ninety_Nine_Bottles_of_Beer() {
-    return 'Beer function';
+  for (beer=99; beer>2 ; beer--) {
+        console.log(
+          beer + " bottles of beer on the wall, " +
+          beer + " bottles of beer. Take one down and pass it around, " +
+          (beer-1) + " bottles of beer on the wall." + "<br>"
+        );
+        }
+        console.log('2	bottles	of	beer	on	the	wall,	2	bottles	of	beertake	one	down,	pass	it	around,	1	bottle	of	beer	on	the	wall.');
+        console.log('1	bottle	of	beer	on	the	wall,	1	bottle	of	beer take	one	down,	pass	it	around,	no	more	bottles	of	beer	on	the	wall.');
 }
